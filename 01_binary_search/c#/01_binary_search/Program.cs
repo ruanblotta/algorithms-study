aa0fb7c635cf99b788b52eb514603962c24e9434
Ruan Blotta <ruanblotta@gmail.com>
2024-04-02 14:03:36 -0300

    Solving Challenge 01-Binary Research in C#

---
+++ 01_binary_search/c#/01_binary_search/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchMain
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            var my_list = new List<int> {1,3,5,7,9};
+            Console.WriteLine(Binary_search(my_list, 3));
+            Console.WriteLine(Binary_search(my_list, - 1));            
+        }
+
+        private static int? Binary_search(IList<int> list, int item)
+            {
+                var low = 0;
+                var high = list.Count() -1;
+
+                 while (low <= high)
+                {
+                    var mid = (low + high) / 2;
+                    var guess = list[mid];
+                    if (guess == item)
+                    {
+                        return mid;
+                    }
+                    if (guess > item)
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+                return null;
+            }         
+    }
+}
